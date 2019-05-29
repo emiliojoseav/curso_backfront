b955d38d0732b15c8fbc4781331b139d9cd44cb6
emiliojoseav <emiliojoseav@gmail.com>
2019-05-29 21:58:39 +0200

    se incluyen ficheros descartados por 'gitignore'

---
+++ symfony/var/cache/prod/twig/28/2888624af5505e07eb0616a52ca709c09810230d7113bebe4f54193128728923.php
@@ -0,0 +1,56 @@
+<?php
+
+use Twig\Environment;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Markup;
+use Twig\Sandbox\SecurityError;
+use Twig\Sandbox\SecurityNotAllowedTagError;
+use Twig\Sandbox\SecurityNotAllowedFilterError;
+use Twig\Sandbox\SecurityNotAllowedFunctionError;
+use Twig\Source;
+use Twig\Template;
+
+/* BackendBundle:Default:index.html.twig */
+class __TwigTemplate_6fb995d2d62be07747a406aa2b1710602acdce77edabcde39c551acf415dbcc1 extends \Twig\Template
+{
+    public function __construct(Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->parent = false;
+
+        $this->blocks = [
+        ];
+    }
+
+    protected function doDisplay(array $context, array $blocks = [])
+    {
+        // line 1
+        echo "Hello World!
+";
+    }
+
+    public function getTemplateName()
+    {
+        return "BackendBundle:Default:index.html.twig";
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  30 => 1,);
+    }
+
+    /** @deprecated since 1.27 (to be removed in 2.0). Use getSourceContext() instead */
+    public function getSource()
+    {
+        @trigger_error('The '.__METHOD__.' method is deprecated since version 1.27 and will be removed in 2.0. Use getSourceContext() instead.', E_USER_DEPRECATED);
+
+        return $this->getSourceContext()->getCode();
+    }
+
+    public function getSourceContext()
+    {
+        return new Source("", "BackendBundle:Default:index.html.twig", "C:\\wamp\\www\\curso-backfront\\symfony\\src\\BackendBundle/Resources/views/Default/index.html.twig");
+    }
+}
